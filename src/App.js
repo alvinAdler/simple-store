bef2da37ec736651d3fc93297d75139ad5870dbe
alvinAdler <alvinadlersampow@gmail.com>
2021-07-12 11:34:59 +0700

    Added top and bottom wavy banner

--- src/App.js
+++ src/App.js
@@ -21,13 +21,20 @@ function App () {
     })
   }, [])
 
-    return (
+    return (    
       <div className="App container">
+        <div className="sample-header jumbotron">
+          <h1 className="display-4">Simple Store</h1>
+          <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab dolor laudantium dolorem suscipit consequuntur maxime numquam repudiandae perferendis reiciendis voluptatibus, doloremque iusto assumenda pariatur iste minus eos sequi? Animi, ullam!</p>
+          <hr className="my-4" />
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit laborum cupiditate quaerat tempore perspiciatis suscipit neque autem fuga veniam non velit, sequi facilis commodi harum laboriosam optio. Consequatur, optio reprehenderit?</p>
+        </div>
         {items.map((item, index) => {
           return(
             <Item key={index} itemDetails={item}/>
           )
         })}
+        
       </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <svg style={{position: "absolute", top: 0, right: 0, left: 0}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#dbdad5" fillOpacity="1" d="M0,128L60,149.3C120,171,240,213,360,202.7C480,192,600,128,720,85.3C840,43,960,21,1080,37.3C1200,53,1320,107,1380,133.3L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
     <App />
+    <svg style={{position: "relative", bottom: 0, right: 0, left: 0}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#dbdad5" fillOpacity="1" d="M0,128L60,149.3C120,171,240,213,360,202.7C480,192,600,128,720,85.3C840,43,960,21,1080,37.3C1200,53,1320,107,1380,133.3L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
   </React.StrictMode>,
   document.getElementById('root')
 );
